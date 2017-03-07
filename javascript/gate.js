0b8d32f5dd1c574551c95f53238788ded3182d1d
Lumater <lumaterlord@gmail.com>
2017-03-07 12:32:40 -0800

    Created

---
+++ javascript/gate.js
@@ -0,0 +1,7 @@
+var psword = document.getElementById("psword").value;
+if (psword == "nox") {
+    window.location.href = "/gate/noxgate.html
+    alert("ACCESS GRANTED");
+} else {
+    alert("ACCESS DENIED");
+}
